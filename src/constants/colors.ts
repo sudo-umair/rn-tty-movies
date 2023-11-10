bafde7b0f1fd92274cbe72187b744cf29a9cf06a
Muhammad Umair <muhammadumair523@gmail.com>
2023-11-10 12:49:50 +0500

    added colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export enum Colors {
+  dark = '#2e2739',
+  white = '#f6f6fa',
+  darkGray = '#827d88',
+  blue = '#61c3f2',
+  lightGray = '#dbdbdf',
+  green = '#15d2bc',
+  pink = '#e26ca5',
+  purple = '#564ca3',
+  yellow = '#cd9d0f',
+}
