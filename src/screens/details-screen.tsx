e59bb1ddfac0a3d98973d3133748999fba6f66c4
Muhammad Umair <muhammadumair523@gmail.com>
2023-11-12 01:39:21 +0500

    use expo-image instead of image background component

--- src/screens/details-screen.tsx
+++ src/screens/details-screen.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { DetailsScreenProps } from '@/interfaces/screens';
 import { constructImageUrl, findGenres } from '@/utils/common';
 import { Colors } from '@/constants/colors';
@@ -7,9 +7,11 @@ import Header from '@/components/ui/header';
 import Button from '@/components/ui/button';
 import { FontFamily, FontSize } from '@/constants/fonts';
 import dayjs from 'dayjs';
+import { Image } from 'expo-image';
 import { Entypo } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import PillContainer from '@/components/ui/pill-container';
+import { blurhash } from '@/constants/data';
 
 var customParseFormat = require('dayjs/plugin/customParseFormat');
 dayjs.extend(customParseFormat);
@@ -25,15 +27,19 @@ const DetailsScreen: React.FC<DetailsScreenProps> = ({ navigation, route }) => {
     <View style={styles.root}>
       <Header style={styles.header} iconStyle={styles.headerText} textStyle={styles.headerText} text={'Watch'} />
       <ScrollView style={styles.root}>
-        <ImageBackground
-          source={{
-            uri: constructImageUrl(item.poster_path),
-          }}
-          resizeMode='cover'
-          style={styles.imageContainer}
-        >
+        <View>
+          <Image
+            source={{
+              uri: constructImageUrl(item.poster_path),
+            }}
+            cachePolicy={'disk'}
+            placeholder={blurhash}
+            contentFit='cover'
+            transition={1000}
+            style={styles.imageContainer}
+          />
           <LinearGradient
-            style={{ ...styles.imageContainer, height: '100%', width: '100%' }}
+            style={[styles.imageContainer, styles.absolute]}
             colors={['transparent', 'rgba(0, 0, 0, 0.3)', 'rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0, 0.8)']}
           >
             <View style={styles.container}>
@@ -47,7 +53,7 @@ const DetailsScreen: React.FC<DetailsScreenProps> = ({ navigation, route }) => {
               />
             </View>
           </LinearGradient>
-        </ImageBackground>
+        </View>
         <View style={styles.detailsContainer}>
           <PillContainer data={findGenres(item.genre_ids, item.media_type)} section='Genres' />
           <Text style={styles.heading}>OverView</Text>
@@ -81,6 +87,16 @@ const styles = StyleSheet.create({
     height: 450,
     width: '100%',
     flexDirection: 'row',
+    borderWidth: 1,
+  },
+  absolute: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
   container: {
     alignSelf: 'flex-end',
